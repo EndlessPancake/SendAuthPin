af2c7bfb01e8562690e95bfe4528a25839262cb4
Ubuntu <ubuntu@ip-172-31-34-203.ec2.internal>
2017-12-13 02:50:44 +0000

    add Google/Datastore Save function

--- index.js
+++ index.js
@@ -4,6 +4,23 @@
 var gpc = require('generate-pincode')
 var pin = gpc(4)
 
+// 
+// Google Datastore
+//
+const Datastore = require('@google-cloud/datastore');
+const projectId = 'endlesspancake4u-184808';
+// // Instantiates a client
+const datastore = Datastore({
+  projectId: projectId
+});
+
+
+// // parameters
+// const timeInMs = Date.now();
+const timeInMs = Date();
+const kind = 'Task';
+const name = 'pin_4_digit';
+const taskKey = datastore.key([kind, name]);
 //
 // Sending and receiving data in JSON format using POST method
 //
@@ -31,3 +48,29 @@ xhr.onreadystatechange = function () {
 var data = JSON.stringify({ value1: + pin });
 xhr.send(data);
 // console.log(data);
+
+// and Saves the entity
+const task = {
+  key: taskKey,
+  data: {
+    local_time: timeInMs.toString(),
+    your_number: pin
+  }
+};
+
+datastore.save(task)
+  .then(() => {
+    console.log(`Saved ${task.key.name}: ${task.data.your_number}`);
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
+
+// get pin
+datastore.get(taskKey)
+  .then((results) => {
+    // Task found.
+    const entity = results[0];
+    console.log('Result:', entity);
+  });
+
